9e362866d9a1914cde5d4fffcc566d4e3beac8d0
Ale Pereira <naitwa-alexandre@gmail.com>
2026-06-17 12:18:10 -0300

    feat: fast array serializer class created

--- Plugins/Inventory/Source/Inventory/Private/InventoryManagement/FastArray/Inv_FastArray.cpp
+++ Plugins/Inventory/Source/Inventory/Private/InventoryManagement/FastArray/Inv_FastArray.cpp
@@ -1,1 +1,25 @@
 ﻿#include "InventoryManagement/FastArray/Inv_FastArray.h"
+
+TArray<UInv_InventoryItem*> FInv_InventoryFastArray::GetAllItems() const
+{
+}
+
+void FInv_InventoryFastArray::PreReplicatedRemove(const TArray<int32> RemovedIndices, int32 FinalSize)
+{
+}
+
+void FInv_InventoryFastArray::PostReplicatedAdd(const TArrayView<int32> AddedIndices, int32 FinalSize)
+{
+}
+
+UInv_InventoryItem* FInv_InventoryFastArray::AddEntry(UInv_InventoryComponent* ItemComponent)
+{
+}
+
+UInv_InventoryItem* FInv_InventoryFastArray::AddEntry(UInv_InventoryItem* Item)
+{
+}
+
+void FInv_InventoryFastArray::RemoveEntry(UInv_InventoryItem* Item)
+{
+}
