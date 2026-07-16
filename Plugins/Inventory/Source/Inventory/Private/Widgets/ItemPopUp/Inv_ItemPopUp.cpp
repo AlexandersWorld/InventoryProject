325634d3af4ba9846ae5dfabfbedac189ddda331
Ale Pereira <naitwa-alexandre@gmail.com>
2026-07-16 12:03:55 -0300

    feat: item popup delegates

--- Plugins/Inventory/Source/Inventory/Private/Widgets/ItemPopUp/Inv_ItemPopUp.cpp
+++ Plugins/Inventory/Source/Inventory/Private/Widgets/ItemPopUp/Inv_ItemPopUp.cpp
@@ -2,3 +2,51 @@
 
 
 #include "Widgets/ItemPopUp/Inv_ItemPopUp.h"
+
+#include "Components/Button.h"
+#include "Components/Slider.h"
+
+
+void UInv_ItemPopUp::NativeOnInitialized()
+{
+	Super::NativeOnInitialized();
+	
+	Button_Split->OnClicked.AddDynamic(this, &ThisClass::SplitButtonClicked);
+	Button_Drop->OnClicked.AddDynamic(this, &ThisClass::DropButtonClicked);
+	Button_Consume->OnClicked.AddDynamic(this, &ThisClass::ConsumeButtonClicked);
+	Slider_Split->OnValueChanged.AddDynamic(this, &ThisClass::UInv_ItemPopUp::SliderValueChanged);
+}
+
+int32 UInv_ItemPopUp::GetSplitAmount() const
+{
+	return FMath::Floor(Slider_Split->GetValue());
+}
+
+void UInv_ItemPopUp::SplitButtonClicked()
+{
+	if  (OnSplit.ExecuteIfBound(GetSplitAmount(), GridIndex))
+	{
+		RemoveFromParent();
+	}
+}
+
+void UInv_ItemPopUp::DropButtonClicked()
+{
+	if (OnDrop.ExecuteIfBound(GridIndex))
+	{
+		RemoveFromParent();
+	}
+}
+
+void UInv_ItemPopUp::ConsumeButtonClicked()
+{
+	if (OnConsume.ExecuteIfBound(GridIndex))
+	{
+		RemoveFromParent();
+	}
+}
+
+void UInv_ItemPopUp::SliderValueChanged(float Value)
+{
+	
+}
--- Plugins/Inventory/Source/Inventory/Public/Widgets/ItemPopUp/Inv_ItemPopUp.h
+++ Plugins/Inventory/Source/Inventory/Public/Widgets/ItemPopUp/Inv_ItemPopUp.h
@@ -14,11 +14,24 @@ class USizeBox;
  * The item popup widget shows up when right-clicking on an item
  * in the inventory grid.
  */
+
+DECLARE_DYNAMIC_DELEGATE_TwoParams(FPopUpMenuSplit, int32, SplitAmount, int32, Index);
+DECLARE_DYNAMIC_DELEGATE_OneParam(FPopUpMenuDrop, int32, Index);
+DECLARE_DYNAMIC_DELEGATE_OneParam(FPopUpMenuConsume, int32, Index);
+
 UCLASS()
 class INVENTORY_API UInv_ItemPopUp : public UUserWidget
 {
 	GENERATED_BODY()
 	
+public:
+	virtual void NativeOnInitialized() override;
+	
+	FPopUpMenuSplit OnSplit;
+	FPopUpMenuDrop OnDrop;
+	FPopUpMenuConsume OnConsume;
+	
+	int32 GetSplitAmount() const;
 private:
 	
 	UPROPERTY(meta=(BindWidget))
@@ -38,4 +51,19 @@ private:
 	
 	UPROPERTY(meta=(BindWidget))
 	TObjectPtr<USizeBox> SizeBox_Root;
+	
+	int32 GridIndex {INDEX_NONE};
+	
+	UFUNCTION()
+	void SplitButtonClicked();
+
+	UFUNCTION()
+    void DropButtonClicked();
+    
+    UFUNCTION()
+    void ConsumeButtonClicked();
+	
+	UFUNCTION()
+	void SliderValueChanged(float Value);
+	
 };
