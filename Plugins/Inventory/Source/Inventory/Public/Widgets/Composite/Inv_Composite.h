c58b5a0847da9d5c6266171edb0b41e1b366ba91
Ale Pereira <naitwa-alexandre@gmail.com>
2026-07-18 14:04:44 -0300

    feat: creates composite base class and its children

---
+++ Plugins/Inventory/Source/Inventory/Public/Widgets/Composite/Inv_Composite.h
@@ -0,0 +1,26 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Inv_CompositeBase.h"
+#include "Inv_Composite.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class INVENTORY_API UInv_Composite : public UInv_CompositeBase
+{
+	GENERATED_BODY()
+	
+public:
+	virtual void ApplyFunction(FuncType Function) override;
+	virtual void Collapse() override;
+	
+protected:
+	virtual void NativeOnInitialized() override;
+	
+private:
+	TArray<TObjectPtr<UInv_CompositeBase>> Children;
+};
